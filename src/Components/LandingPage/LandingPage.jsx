0965f7ae6de4620c490842e7aea816a90b0da0e0
Marco-Pugliese <marco.pugliese92@yahoo.com>
2025-02-05 09:59:04 +0100

    upd

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,22 +1,39 @@
-import { Col, Row } from "react-bootstrap";
+import { Carousel, Col, Row } from "react-bootstrap";
 
 const LandingPage = () => {
   return (
-    <div id="landingPage">
-      <Row className="text-center fs-4 mt-3 fade-in">
-        <Col className="col-12">Benvenuto su</Col>
-        <Col className="fs-1 py-2 fade-in">FP Computers</Col>
-      </Row>
-      <Row className="text-center fs-4 mt-3 mt-4">
-        <Col className="col-12 mt-4 p-4">
-          <div className="px-4">
-            Sei stato contattato e stai ricevendo assistenza?
-          </div>
-          <div className="bg-purple btn m-2 p-2 fs-5 slide-in-top">
-            <a href="https://www.supremocontrol.com/download.aspx?file=Supremo.exe&id_sw=7&ws=supremocontrol.com">
-              Clicca qui!
-            </a>
-          </div>
+    <div
+      id="landingPage"
+      className="d-flex align-items-center flex-column justify-content-center"
+    >
+      <Row className="text-center justify-content-center fs-4 mt-4 ">
+        <Col className="mt-5 bg-light text-dark border text-shadow border-white rounded-2 shadow-lg px-2 py-5">
+          <Carousel fade className="w-100">
+            <Carousel.Item className="w-100">
+              <div className="p-4 fs-4">Benvenuto su</div>
+              <div className="fs-1 slide-in-top p-4 words-cng">
+                FP Computers
+              </div>
+            </Carousel.Item>
+            <Carousel.Item className="w-100">
+              <div className="p-4">
+                Sei stato contattato e stai ricevendo assistenza?
+              </div>
+              <div className="bg-purple btn mx-2 my-4 p-2 fs-5 slide-in-top shadow-btn notxt-shdw">
+                <a href="https://www.supremocontrol.com/download.aspx?file=Supremo.exe&id_sw=7&ws=supremocontrol.com">
+                  Clicca qui!
+                </a>
+              </div>
+            </Carousel.Item>
+            <Carousel.Item className="w-100">
+              <div className="p-4">
+                Vuoi contattarci per richiedere assistenza?
+              </div>
+              <div className="bg-purple btn mx-2 my-4 p-2 fs-5 slide-in-top shadow-btn notxt-shdw">
+                <a className="text-light">Clicca qui!</a>
+              </div>
+            </Carousel.Item>
+          </Carousel>
         </Col>
       </Row>
     </div>
