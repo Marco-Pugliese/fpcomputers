fd58ae21cf06d86fb9d186b884ee4d0750d1ed56
Marco-Pugliese <marco.pugliese92@yahoo.com>
2025-02-12 18:43:41 +0100

    needs supportPage

--- src/Components/ServicePage/ServicePage.jsx
+++ src/Components/ServicePage/ServicePage.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const ServicePage = () => {
+  const [support, isSupportClicked] = useState(false);
+  const [azienda, isAziendaClicked] = useState(false);
+  const [assistenza, isAssistenzaClicked] = useState(false);
+  const [vendita, isVenditaClicked] = useState(false);
   return (
     <div id="ServicePage">
       <Container>
-        <Row>
-          <Col className="col-6 spacing1 d-flex flex-column">
+        <Row
+          className="d-flex align-items-center border shad-wht bg-sm-1"
+          onClick={() => {
+            isVenditaClicked(!vendita);
+          }}
+        >
+          {vendita && (
+            <Col className="position-absolute w-100 h-100 bg-black">
+              <div className="h-100 w-100 d-flex align-items-center justify-content-center">
+                <Link to={"/Support"} className="nav-link">
+                  <div className="bg-success py-2 px-3 rounded-4 scale-on-hover">
+                    Chiedi Informazioni
+                  </div>
+                </Link>
+              </div>
+            </Col>
+          )}
+          <Col className="col-12 col-lg-6 darker-sm text-center text-lg-end h-30vh spacing1 py-4 d-flex flex-column justify-content-center">
             <div className="fs-3">Vendita di Prodotti</div>
             <div className="small d-flex flex-column">
               <div>PC e Periferiche</div>
@@ -13,12 +35,28 @@ const ServicePage = () => {
               <div>Reti e Connettività</div>
             </div>
           </Col>
-          <Col></Col>
+          <Col className="d-none d-lg-block h-30vh bg1 "></Col>
         </Row>
-        <Row>
-          <Col></Col>
-          <Col className="col-6 spacing1 d-flex flex-column">
-            <div className="fs-3">Assistenza Tecnica</div>
+        <Row
+          className="d-flex align-items-center border shad-wht bg-sm-2 position-relative"
+          onClick={() => {
+            isAssistenzaClicked(!assistenza);
+          }}
+        >
+          {assistenza && (
+            <Col className="position-absolute w-100 h-100 bg-black">
+              <div className="h-100 w-100 d-flex align-items-center justify-content-center">
+                <Link to={"/Support"} className="nav-link">
+                  <div className="bg-primary py-2 px-3 rounded-4 scale-on-hover">
+                    Chiedi Assistenza
+                  </div>
+                </Link>
+              </div>
+            </Col>
+          )}
+          <Col className="d-none d-lg-block h-30vh bg2"></Col>
+          <Col className="col-12 col-lg-6  darker-sm text-center text-lg-start h-30vh spacing1 py-4 d-flex flex-column justify-content-center">
+            <div className="fs-3">Assistenza Tecnica</div>{" "}
             <div className="small d-flex flex-column">
               <div>Riparazioni e Manutenzione</div>
               <div>Configurazione Software</div>
@@ -26,9 +64,24 @@ const ServicePage = () => {
             </div>
           </Col>
         </Row>
-        <Row>
-          <Col className="col-6 spacing1">Servizi per Aziende</Col>
-          <Col className="col-6 spacing1 d-flex flex-column">
+        <Row
+          className="d-flex align-items-center border shad-wht bg-sm-3 position-relative"
+          onClick={() => {
+            isAziendaClicked(!azienda);
+          }}
+        >
+          {azienda && (
+            <Col className="position-absolute w-100 h-100 bg-black">
+              <div className="h-100 w-100 d-flex align-items-center justify-content-center">
+                <Link to={"/Support"} className="nav-link">
+                  <div className="bg-danger py-2 px-3 rounded-4 scale-on-hover">
+                    Chiedi Consulenza
+                  </div>
+                </Link>
+              </div>
+            </Col>
+          )}
+          <Col className="col-12 col-lg-6 darker-sm text-center text-lg-end h-30vh spacing1 py-4 d-flex flex-column justify-content-center">
             <div className="fs-3"> Servizi per Aziende</div>
             <div className="d-flex flex-column">
               <div>Networking e Sicurezza</div>
@@ -36,11 +89,28 @@ const ServicePage = () => {
               <div> Soluzioni Cloud e Backup</div>
             </div>
           </Col>
+          <Col className="d-none d-lg-block h-30vh bg3"></Col>
         </Row>
-        <Row>
-          <Col></Col>
-          <Col className="col-6 spacing1 d-flex flex-column">
-            <div>Supporto e Consulenza</div>
+        <Row
+          className="d-flex align-items-center border bg-sm-4 shad-wht position-relative"
+          onClick={() => {
+            isSupportClicked(!support);
+          }}
+        >
+          {support && (
+            <Col className="position-absolute w-100 h-100 bg-black">
+              <div className="h-100 w-100 d-flex align-items-center justify-content-center">
+                <Link to={"/Support"} className="nav-link">
+                  <div className="bg-warning py-2 px-3 rounded-4 scale-on-hover">
+                    Chiedi Supporto
+                  </div>
+                </Link>
+              </div>
+            </Col>
+          )}
+          <Col className="d-none d-lg-block h-30vh bg4"></Col>
+          <Col className="col-12 col-lg-6 darker-sm text-center text-lg-start h-30vh spacing1 py-4 d-flex flex-column justify-content-center">
+            <div className="fs-3">Supporto e Consulenza</div>
             <div className="d-flex flex-column">
               <div>Assistenza Remota</div>
               <div>Consulenza per Acquisti</div>
